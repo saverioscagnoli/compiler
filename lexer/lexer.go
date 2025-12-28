d0bb64946684fd8e0de7c99ca8d69dd5de47224b
Leone <leone.pasqualucci@gmail.com>
2025-12-28 17:11:04 +0100

    lexer impl 0.1

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,39 +1,5 @@
 package lexer
 
-type Word struct {
-	Value string
-	Type  WordType
-}
-
-type WordType int
-
-const (
-	Number WordType = iota
-	String
-	Symbol
-)
-
-func (w *Word) append(ch byte) {
-	w.Value += string(ch)
-	if isSymbol(ch) {
-		w.Type = Symbol
-	} else if !isNumber(ch) {
-		w.Type = String
-	}
-}
-
-func isNumber(ch byte) bool {
-	return ch >= '0' && ch <= '9'
-}
-
-func isSymbol(ch byte) bool {
-	return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '='
-}
-
-func isSeparetor(ch byte) bool {
-	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
-}
-
 type Lexer struct {
 	input        string
 	position     int
@@ -57,27 +23,6 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
-func decodeSymbol(word Word) Token {
-	switch word.Value {
-	case "+":
-		return Token{Type: PLUS, Literal: "+"}
-	case "-":
-		return Token{Type: MINUS, Literal: "-"}
-	case "*":
-		return Token{Type: MULTIPLY, Literal: "*"}
-	case "/":
-		return Token{Type: DIVIDE, Literal: "/"}
-	case "=":
-		return Token{Type: EQUAL, Literal: "="}
-	default:
-		return Token{Type: ILLEGAL, Literal: word.Value}
-	}
-}
-
-func decodeString(word Word) Token {
-	return Token{Type: STRING, Literal: word.Value}
-}
-
 func (l *Lexer) Tokenize() []Token {
 	var tokens []Token
 	var word Word
@@ -88,20 +33,21 @@ func (l *Lexer) Tokenize() []Token {
 
 		if len(word.Value) > 0 || i == len(l.input)-1 {
 			var sim bool = isSymbol(l.ch)
+			var num bool = isNumber(l.ch)
 			switch word.Type {
 			case Symbol:
 				if !sim || sep {
-					tokens = append(tokens, decodeSymbol(word))
+					tokens = append(tokens, decodeToken(word.Value))
 					word = Word{}
 				}
 			case Number:
-				if !isNumber(l.ch) || sep {
+				if !num || sep {
 					tokens = append(tokens, Token{Type: NUMBER, Literal: word.Value})
 					word = Word{}
 				}
 			case String:
 				if sim || sep {
-					tokens = append(tokens, decodeString(word))
+					tokens = append(tokens, decodeToken(word.Value))
 					word = Word{}
 				}
 			}
--- lexer/token.go
+++ lexer/token.go
@@ -5,16 +5,67 @@ type Token struct {
 	Literal string
 }
 
-type TokenType int
+type TokenType string
 
 const (
-	ILLEGAL = iota
-	EOF
-	STRING
-	NUMBER
-	PLUS
-	MINUS
-	MULTIPLY
-	DIVIDE
-	EQUAL
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF     TokenType = "EOF"
+	STRING  TokenType = "STRING"
+	NUMBER  TokenType = "NUMBER"
+	PLUS    TokenType = "PLUS"
+	MINUS   TokenType = "MINUS"
+	STAR    TokenType = "STAR"
+	SLASH   TokenType = "SLASH"
+	ASSIGN  TokenType = "ASSIGN"
+	VAR     TokenType = "VAR"
 )
+
+type Word struct {
+	Value string
+	Type  WordType
+}
+
+type WordType int
+
+const (
+	Number WordType = iota
+	String
+	Symbol
+)
+
+func (w *Word) append(ch byte) {
+	w.Value += string(ch)
+	if isSymbol(ch) {
+		w.Type = Symbol
+	} else if !isNumber(ch) {
+		w.Type = String
+	}
+}
+
+func isNumber(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func isSymbol(ch byte) bool {
+	return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '='
+}
+
+func isSeparetor(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
+}
+
+var STATIC_TOKENS = map[string]TokenType{
+	"+":   PLUS,
+	"-":   MINUS,
+	"*":   STAR,
+	"/":   SLASH,
+	"=":   ASSIGN,
+	"var": VAR,
+}
+
+func decodeToken(s string) Token {
+	if tokenType, exists := STATIC_TOKENS[s]; exists {
+		return Token{Type: tokenType, Literal: s}
+	}
+	return Token{Type: STRING, Literal: s}
+}
